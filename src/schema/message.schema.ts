fb4b7e7d817117264ad97f6205a3553245d87cf8
Muhammad Ahyaruddin <ahyaruddin07@gmail.com>
2024-12-25 23:22:24 +0700

    send image schema

--- src/schema/message.schema.ts
+++ src/schema/message.schema.ts
@@ -1,28 +1,36 @@
 import {
-	isJidBroadcast,
-	isJidGroup,
-	isJidNewsletter,
-	isJidStatusBroadcast,
-	isJidUser,
+  isJidBroadcast,
+  isJidGroup,
+  isJidNewsletter,
+  isJidStatusBroadcast,
+  isJidUser,
 } from "@whiskeysockets/baileys";
 import { z } from "zod";
 
 export const jidSchema = z
-	.string()
-	.refine(
-		(jid) =>
-			!/\s/.test(jid) ||
-			isJidUser(jid) ||
-			isJidGroup(jid) ||
-			isJidBroadcast(jid) ||
-			isJidNewsletter(jid) ||
-			isJidStatusBroadcast(jid),
-		"Invalid jid"
-	);
+  .string()
+  .refine(
+    (jid) =>
+      !/\s/.test(jid) ||
+      isJidUser(jid) ||
+      isJidGroup(jid) ||
+      isJidBroadcast(jid) ||
+      isJidNewsletter(jid) ||
+      isJidStatusBroadcast(jid),
+    "Invalid jid"
+  );
 
 export const textMessageSchema = z.object({
-	text: z.string(),
-	jid: jidSchema,
+  text: z.string(),
+  jid: jidSchema,
 });
 
 export type ITextMessage = z.infer<typeof textMessageSchema>;
+
+export const imageMessageSchema = z.object({
+  file: z.instanceof(File),
+  jid: jidSchema,
+  caption: z.string().optional(),
+});
+
+export type IImageMessage = z.infer<typeof imageMessageSchema>;
