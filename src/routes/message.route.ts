0139e71525f3efe4fa5f669dc786c6344cf22aca
Muhammad Ahyaruddin <ahyaruddin07@gmail.com>
2024-12-25 23:22:37 +0700

    remove unnecessary

--- src/routes/message.route.ts
+++ src/routes/message.route.ts
@@ -1,7 +1,7 @@
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { messageController } from "../controllers/index.js";
-import { imageMessageSchema, textMessageSchema } from "../schema/message.schema.js";
+import { textMessageSchema } from "../schema/message.schema.js";
 
 const messageRoute = new Hono();
 
